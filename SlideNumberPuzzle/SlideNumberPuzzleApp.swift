99504997f584c59f0a3fe5c0ac66e02d3c5b458a
Pradipat <P0861719933@gmail.com>
2023-11-19 13:38:50 +0700

    Initial Commit

---
+++ SlideNumberPuzzle/SlideNumberPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideNumberPuzzleApp.swift
+//  SlideNumberPuzzle
+//
+//  Created by Pradipat Jareanporn on 19/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SlideNumberPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
